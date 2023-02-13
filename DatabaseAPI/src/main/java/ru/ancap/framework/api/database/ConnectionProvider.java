f717efe9a8ff6cda05baa1665d8f716aa5623a2c
ancap-kun <90708073+ancap-kun@users.noreply.github.com>
2023-02-13 12:06:38 +0500

    Delete DatabaseAPI directory

--- DatabaseAPI/src/main/java/ru/ancap/framework/api/database/ConnectionProvider.java
+++
@@ -1,8 +0,0 @@
-package ru.ancap.framework.api.database;
-
-import java.sql.Connection;
-import java.util.function.Supplier;
-
-public interface ConnectionProvider extends Supplier<Connection> {
-
-}
--- DatabaseAPI/src/main/java/ru/ancap/framework/api/function/Holder.java
+++
@@ -1,9 +0,0 @@
-package ru.ancap.framework.api.function;
-
-import java.util.function.Supplier;
-
-public interface Holder<T> {
-
-    T get(Supplier<T> initializer);
-
-}
