b90bd5fb91508c5e4e7f3f26021d880863952d38
ancap-kun <90708073+ancap-kun@users.noreply.github.com>
2023-02-13 12:05:42 +0500

    Delete AncapPluginAPI directory

--- AncapPluginAPI/src/main/java/ru/ancap/framework/plugin/api/exception/CommandNotRegisteredException.java
+++
@@ -1,9 +0,0 @@
-package ru.ancap.framework.plugin.api.exception;
-
-public class CommandNotRegisteredException extends RuntimeException {
-
-    public CommandNotRegisteredException(String s) {
-        super(s);
-    }
-    
-}
