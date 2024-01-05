e238d69008a8c5d647ab07401e7710ef0c3a1767
PukPukov <vovanchikputin@vk.com>
2024-01-05 07:10:53 +0500

    Add contract to CallableMessage

--- CommunicatorAPI/src/main/java/ru/ancap/framework/communicate/message/CallableMessage.java
+++ CommunicatorAPI/src/main/java/ru/ancap/framework/communicate/message/CallableMessage.java
@@ -3,8 +3,14 @@ package ru.ancap.framework.communicate.message;
 public interface CallableMessage {
 
     /**
+     * By contract implementation methods should do nothing more that message creation. If possible, method should be pure.
      * @return String in minimessage format.
      */
-    String call(String identifier);
+    String call(String receiverId);
+
+    // Implementations should support proper equals(), hashCode() and toString().
+    boolean equals(Object object);
+    int hashCode();
+    String toString();
     
 }
--- CommunicatorAPI/src/main/java/ru/ancap/framework/communicate/message/WrapperMessage.java
+++ CommunicatorAPI/src/main/java/ru/ancap/framework/communicate/message/WrapperMessage.java
@@ -3,6 +3,9 @@ package ru.ancap.framework.communicate.message;
 import lombok.AllArgsConstructor;
 import lombok.experimental.Delegate;
 
+/**
+ * Since CallableMessage should support toString(), equals() and hashCode() providing lambda to this class constructor is discouraged
+ */
 @AllArgsConstructor
 public class WrapperMessage implements CallableMessage {
     
