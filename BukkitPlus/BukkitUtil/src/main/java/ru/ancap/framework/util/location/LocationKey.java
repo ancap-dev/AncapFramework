6c592095a6177337187446aba96edba069e947ba
PukPukov <vovanchikputin@vk.com>
2024-04-12 17:14:04 +0500

    better location key supporting null worlds

--- BukkitPlus/BukkitUtil/src/main/java/ru/ancap/framework/util/location/LocationKey.java
+++ BukkitPlus/BukkitUtil/src/main/java/ru/ancap/framework/util/location/LocationKey.java
@@ -8,17 +8,25 @@ import org.bukkit.Location;
 public class LocationKey {
     
     public static String get(Location location) {
-        return location.getWorld().getName()+";"+location.getBlockX()+";"+location.getBlockY()+";"+location.getBlockZ();
+        String coords = location.getBlockX()+";"+location.getBlockY()+";"+location.getBlockZ();
+        return location.getWorld() != null ? location.getWorld().getName()+";"+coords : coords;
     }
     
     public static Location ofKey(String key) {
         String[] data = key.split(";");
-        return new Location(
-                Bukkit.getWorld(data[0]),
-                Double.parseDouble(data[1]),
-                Double.parseDouble(data[2]),
-                Double.parseDouble(data[3])
+        if (data.length == 3) return new org.bukkit.Location(
+            null,
+            Double.parseDouble(data[0]),
+            Double.parseDouble(data[1]),
+            Double.parseDouble(data[2])
         );
+        else if (data.length == 4) return new org.bukkit.Location(
+            Bukkit.getWorld(data[0]),
+            Double.parseDouble(data[1]),
+            Double.parseDouble(data[2]),
+            Double.parseDouble(data[3])
+        );
+        else throw new IllegalStateException("Illegal location key: "+key+", show have 3 or 4 entries separated with \";\", but have "+data.length);
     }
     
 }
