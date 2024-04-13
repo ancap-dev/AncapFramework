b20957cc7535ebf7ec6aba8b462995daceaea9b3
PukPukov <vovanchikputin@vk.com>
2024-04-13 09:41:06 +0500

    fix everyone can reload locales

---
+++ AncapPluginAPI/src/main/java/ru/ancap/framework/plugin/api/common/CommonPermissions.java
@@ -0,0 +1,10 @@
+package ru.ancap.framework.plugin.api.common;
+
+import ru.ancap.commons.ImplementationRequired;
+
+@ImplementationRequired
+public class CommonPermissions {
+    
+    public static String reloadLocales = "ancap-framework.reload.locales";
+    
+}
--- AncapPluginAPI/src/main/java/ru/ancap/framework/plugin/api/language/locale/loader/LocaleReloadInput.java
+++ AncapPluginAPI/src/main/java/ru/ancap/framework/plugin/api/language/locale/loader/LocaleReloadInput.java
@@ -1,20 +1,23 @@
 package ru.ancap.framework.plugin.api.language.locale.loader;
 
-import lombok.RequiredArgsConstructor;
-import ru.ancap.framework.command.api.commands.object.event.CommandDispatch;
-import ru.ancap.framework.command.api.commands.object.executor.CommandOperator;
+import ru.ancap.framework.command.api.commands.operator.exclusive.Exclusive;
+import ru.ancap.framework.command.api.commands.operator.exclusive.Pass;
+import ru.ancap.framework.command.api.commands.operator.exclusive.Permission;
 import ru.ancap.framework.language.additional.LAPIMessage;
+import ru.ancap.framework.plugin.api.common.CommonPermissions;
 import ru.ancap.framework.speak.common.CommonMessageDomains;
 
-@RequiredArgsConstructor
-public class LocaleReloadInput implements CommandOperator {
+public class LocaleReloadInput extends Exclusive {
     
-    private final LocaleHandle localeHandle;
+    public LocaleReloadInput(LocaleHandle localeHandle) {
+        this(new Permission(CommonPermissions.reloadLocales), localeHandle);
+    }
     
-    @Override
-    public void on(CommandDispatch dispatch) {
-        this.localeHandle.reload();
-        dispatch.source().communicator().message(new LAPIMessage(CommonMessageDomains.Reload.localesSuccessfullyReloaded));
+    public LocaleReloadInput(Pass pass, LocaleHandle localeHandle) {
+        super(pass, dispatch -> {
+            localeHandle.reload();
+            dispatch.source().communicator().message(new LAPIMessage(CommonMessageDomains.Reload.localesSuccessfullyReloaded));
+        });
     }
     
 }
