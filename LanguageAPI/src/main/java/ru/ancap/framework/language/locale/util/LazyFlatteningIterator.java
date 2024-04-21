bcdf9fc334f3fe0e2cd1663bb1a9320787d9113e
PukPukov <vovanchikputin@vk.com>
2024-04-21 09:27:15 +0500

    fix no targeted fallbacks caused NullPointerException

--- LanguageAPI/src/main/java/ru/ancap/framework/language/locale/util/LazyFlatteningIterator.java
+++ LanguageAPI/src/main/java/ru/ancap/framework/language/locale/util/LazyFlatteningIterator.java
@@ -2,6 +2,8 @@ package ru.ancap.framework.language.locale.util;
 
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.iterators.EmptyIterator;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Iterator;
 import java.util.List;
@@ -11,24 +13,23 @@ import java.util.function.Supplier;
 @RequiredArgsConstructor
 public class LazyFlatteningIterator<E> implements Iterator<E> {
     
-    private final Iterator<Supplier<List<E>>> supplierIterator;
+    private final Iterator<Supplier<@Nullable List<E>>> supplierIterator;
     private Iterator<E> currentIterator = null;
     
     @Override
     public boolean hasNext() {
         while ((this.currentIterator == null || !this.currentIterator.hasNext()) && this.supplierIterator.hasNext()) {
-            this.currentIterator = this.supplierIterator.next().get().iterator();
+            @Nullable List<E> list = this.supplierIterator.next().get();
+            if (list == null) this.currentIterator = EmptyIterator.emptyIterator();
+            else this.currentIterator = list.iterator();
         }
         return this.currentIterator != null && this.currentIterator.hasNext();
     }
     
     @Override
     public E next() {
-        if (hasNext()) {
-            return this.currentIterator.next();
-        } else {
-            throw new NoSuchElementException();
-        }
+        if (hasNext()) return this.currentIterator.next();
+        else throw new NoSuchElementException();
     }
     
 }
