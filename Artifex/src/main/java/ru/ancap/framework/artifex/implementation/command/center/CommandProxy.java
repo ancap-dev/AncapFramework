0cbdea5895bffc32873132c9b5d8ef6999c9e0bb
PukPukov <vovanchikputin@vk.com>
2024-05-23 06:43:41 +0500

    add comment explanation

--- Artifex/src/main/java/ru/ancap/framework/artifex/implementation/command/center/CommandProxy.java
+++ Artifex/src/main/java/ru/ancap/framework/artifex/implementation/command/center/CommandProxy.java
@@ -11,6 +11,8 @@ public class CommandProxy implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, @NonNull String[] arguments) {
+        // server splits commands by " ", but this could be bad if you want more flexible argument splitting,
+        // you should undo what server does and do it yourself
         String line = command.getName() + " " + String.join(" ", arguments);
         Bukkit.getPluginManager().callEvent(new ProxiedCommandEvent(sender, line));
         return true;
