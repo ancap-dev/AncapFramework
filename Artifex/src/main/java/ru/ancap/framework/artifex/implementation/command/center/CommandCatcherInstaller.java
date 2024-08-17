6791ec700965c08e3b453b8d4862f00a2f8d5f9c
PukPukov <vovanchikputin@vk.com>
2024-08-17 05:53:04 +0500

    end trial bipacketlib dependency period

--- Artifex/src/main/java/ru/ancap/framework/artifex/implementation/command/center/CommandCatcherInstaller.java
+++ Artifex/src/main/java/ru/ancap/framework/artifex/implementation/command/center/CommandCatcherInstaller.java
@@ -1,15 +1,12 @@
 package ru.ancap.framework.artifex.implementation.command.center;
 
-import com.comphenix.protocol.ProtocolLibrary;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
-import ru.ancap.framework.artifex.implementation.PacketLibFork;
+import ru.ancap.framework.artifex.implementation.command.center.util.TabPerformanceChecker;
 import ru.ancap.framework.artifex.implementation.command.center.util.catcher.tab.CommonTabCatcher;
 import ru.ancap.framework.artifex.implementation.command.center.util.catcher.tab.PacketEventsTabCatcher;
-import ru.ancap.framework.artifex.implementation.command.center.util.catcher.tab.ProtocolLibTabCatcher;
-import ru.ancap.framework.artifex.implementation.command.center.util.TabPerformanceChecker;
 import ru.ancap.framework.command.api.commands.object.executor.CommandOperator;
 import ru.ancap.framework.command.api.commands.operator.delegate.subcommand.rule.delegate.operate.OperateRule;
 import ru.ancap.framework.plugin.api.AncapPlugin;
@@ -25,16 +22,8 @@ public class CommandCatcherInstaller {
         
         var commonTabCatcher = new CommonTabCatcher(tabPerformanceChecker, global, scope);
         
-        switch (PacketLibFork.CurrentUsage.TAB_COMPLETE_RECEIVE.packetLib()) {
-            case ProtocolLib -> {
-                var protocolLibTabCatcher = new ProtocolLibTabCatcher(owner, commonTabCatcher);
-                ProtocolLibrary.getProtocolManager().addPacketListener(protocolLibTabCatcher);
-            }
-            case PacketEvents -> {
-                var packetEventsTabCatcher = new PacketEventsTabCatcher(commonTabCatcher);
-                PacketEvents.getAPI().getEventManager().registerListener(packetEventsTabCatcher, PacketListenerPriority.LOW);
-            }
-        }
+        var packetEventsTabCatcher = new PacketEventsTabCatcher(commonTabCatcher);
+        PacketEvents.getAPI().getEventManager().registerListener(packetEventsTabCatcher, PacketListenerPriority.LOW);
     }
 
 }
--- Artifex/src/main/java/ru/ancap/framework/artifex/implementation/command/object/PacketLineSpeaker.java
+++ Artifex/src/main/java/ru/ancap/framework/artifex/implementation/command/object/PacketLineSpeaker.java
@@ -1,23 +1,12 @@
 package ru.ancap.framework.artifex.implementation.command.object;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.utility.MinecraftVersion;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerTabComplete;
-import com.mojang.brigadier.LiteralMessage;
-import com.mojang.brigadier.context.StringRange;
-import com.mojang.brigadier.suggestion.Suggestion;
-import com.mojang.brigadier.suggestion.Suggestions;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
-import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
 import org.apiguardian.api.API;
 import org.bukkit.entity.Player;
-import ru.ancap.framework.artifex.implementation.PacketLibFork;
 import ru.ancap.framework.command.api.commands.object.conversation.CommandLineSpeaker;
 import ru.ancap.framework.command.api.commands.object.conversation.CommandSource;
 import ru.ancap.framework.command.api.commands.object.dispatched.InlineTextCommand;
@@ -31,56 +20,20 @@ public class PacketLineSpeaker implements CommandLineSpeaker {
     
     @API(status = API.Status.STABLE)
     public static void sendTabPacket(Player player, int transactionID, InlineTextCommand command, TabBundle tab) {
-        switch (PacketLibFork.CurrentUsage.TAB_COMPLETE_SEND.packetLib()) {
-            case ProtocolLib -> {
-                ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
-                
-                PacketContainer packet = new PacketContainer(PacketType.Play.Server.TAB_COMPLETE);
-                
-                if (protocolManager.getMinecraftVersion().compareTo(new MinecraftVersion("1.13")) >= 0) {
-                    packet.getIntegers().write(0, transactionID);
-                    
-                    StringRange range = new StringRange(
-                        command.argumentStart(tab.argumentsToReplace()) + 1,
-                        command.argumentsEnd() + 1
-                    );
-                    
-                    Suggestions suggestions = new Suggestions(range, tab.tabCompletions().stream()
-                        .map(completion -> new Suggestion(range,
-                            completion.completion(),
-                            completion.tooltipState().map(component -> new LiteralMessage(
-                                JSONComponentSerializer.json().serialize(component)
-                            )).orElse(null)
-                        )).toList());
-                    
-                    packet.getSpecificModifier(Suggestions.class).write(0, suggestions);
-                } else {
-                    String[] completions = new String[tab.tabCompletions().size()];
-                    
-                    for (int i = 0; i < tab.tabCompletions().size(); i++)
-                        completions[i] = tab.tabCompletions().get(i).completion();
-                    packet.getStringArrays().write(0, completions);
-                }
-                
-                protocolManager.sendServerPacket(player, packet);
-            }
-            case PacketEvents -> {
-                WrapperPlayServerTabComplete.CommandRange commandRange = new WrapperPlayServerTabComplete.CommandRange(
-                    command.argumentStart(tab.argumentsToReplace()) + 1,
-                    command.argumentsEnd() + 1
-                );
-                
-                List<WrapperPlayServerTabComplete.CommandMatch> commandMatches = tab.tabCompletions().stream()
-                    .map(ancapCompletion -> new WrapperPlayServerTabComplete.CommandMatch(
-                        ancapCompletion.completion(),
-                        ancapCompletion.tooltipState().orElse(null)
-                    ))
-                    .toList();
-                
-                WrapperPlayServerTabComplete packet = new WrapperPlayServerTabComplete(transactionID, commandRange, commandMatches);
-                PacketEvents.getAPI().getPlayerManager().sendPacket(player, packet);
-            }
-        }
+        WrapperPlayServerTabComplete.CommandRange commandRange = new WrapperPlayServerTabComplete.CommandRange(
+            command.argumentStart(tab.argumentsToReplace()) + 1,
+            command.argumentsEnd() + 1
+        );
+        
+        List<WrapperPlayServerTabComplete.CommandMatch> commandMatches = tab.tabCompletions().stream()
+            .map(ancapCompletion -> new WrapperPlayServerTabComplete.CommandMatch(
+                ancapCompletion.completion(),
+                ancapCompletion.tooltipState().orElse(null)
+            ))
+            .toList();
+        
+        WrapperPlayServerTabComplete packet = new WrapperPlayServerTabComplete(transactionID, commandRange, commandMatches);
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, packet);
     }
     
     private final int transactionID;
