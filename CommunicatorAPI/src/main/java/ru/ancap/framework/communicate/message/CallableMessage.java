11395ecc315c20f5f542e2cea84dde567c87f8ec
PukPukov <vovanchikputin@vk.com>
2024-09-03 12:26:00 +0500

    add convenience methods to CallableMessage

--- CommunicatorAPI/src/main/java/ru/ancap/framework/communicate/message/CallableMessage.java
+++ CommunicatorAPI/src/main/java/ru/ancap/framework/communicate/message/CallableMessage.java
@@ -1,10 +1,23 @@
 package ru.ancap.framework.communicate.message;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import ru.ancap.framework.communicate.communicator.util.CMMSerializer;
+import ru.ancap.framework.identifier.Identifier;
+
 public interface CallableMessage {
 
     /**
      * @return String in minimessage format.
      */
     String call(String identifier);
+    
+    default Component component(Player player) {
+        return this.component(Identifier.of(player));
+    }
+    
+    default Component component(String identifier) {
+        return CMMSerializer.serialize(this.call(identifier));
+    }
     
 }
